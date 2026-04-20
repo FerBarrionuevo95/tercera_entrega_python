d119db7e566928d006e5c18ed7179fcd6788ddab
Fernanda Barrionuevo <fer.barrionuevo95@gmail.com>
2026-04-20 19:03:53 +0200

    buscador agregado

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('crear/', views.crear_post, name='crear'),
+    path('buscar/', views.buscar, name='buscar'),
+
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-from .forms import PostForm
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 def index(request):
     posts = Post.objects.all()
     return render(request, 'app/index.html', {'posts': posts})
+
+
+def crear_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            Post.objects.create(
+                titulo=form.cleaned_data['titulo'],
+                contenido=form.cleaned_data['contenido']
+            )
+            return redirect('index')
+    else:
+        form = PostForm()
+
+    return render(request, 'app/crear.html', {'form': form})
+
+def buscar(request):
+    resultados = []
+
+    if request.GET.get("titulo"):
+        titulo = request.GET.get("titulo")
+        resultados = Post.objects.filter(titulo__icontains=titulo)
+
+    return render(request, 'app/buscar.html', {'resultados': resultados})
